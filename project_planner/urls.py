a6a48721e18206a7af9024823157b06fda154b24
Sebastian Strobl <Sebastian@Strobl.net>
2019-09-09 00:46:57 +0200

    added products and seller api

--- project_planner/urls.py
+++ project_planner/urls.py
@@ -8,11 +8,16 @@ from rest_framework import routers
 from projects import views as project_views
 from bills import views as bill_views
 from profiles import views as profile_views
+from products import views as product_views
+from sellers import views as seller_views
+
 
 router = routers.DefaultRouter()
 router.register('api/projects', project_views.ProjectView)
 router.register('api/bills', bill_views.BillView)
 router.register('api/profiles', profile_views.ProfileView)
+router.register('api/products', product_views.BillView)
+router.register('api/sellers', seller_views.SellerView)
 
 
 def index(request):
@@ -22,8 +27,6 @@ def index(request):
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
-    # url('', include('sellers.urls')),
-    # url('', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
--- sellers/views.py
+++ sellers/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Seller
+from .serializers import SellerSerializer
 
-# Create your views here.
+
+class SellerView(viewsets.ModelViewSet):
+    queryset = Seller.objects.all()
+    serializer_class = SellerSerializer
