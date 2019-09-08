76c66113a88a39e676b5ef45fb593ecbfd0ade39
Sebastian Strobl <Sebastian@Strobl.net>
2019-09-08 23:12:43 +0200

    reconfigured urls

--- bills/views.py
+++ bills/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Bill
+from .serializers import BillSerializer
 
-# Create your views here.
+
+class BillView(viewsets.ModelViewSet):
+    queryset = Bill.objects.all()
+    serializer_class = BillSerializer
--- project_planner/urls.py
+++ project_planner/urls.py
@@ -1,18 +1,28 @@
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.conf.urls import url, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+from rest_framework import routers
 from frontend.views import FrontendRenderView
+from projects import views as project_views
+from bills import views as bill_views
+
+router = routers.DefaultRouter()
+router.register('api/projects', project_views.ProjectView)
+router.register('api/bills', bill_views.BillView)
 
 urlpatterns = [
-    path('', include('frontend.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('projects.urls')),
-    path('', include('bills.urls')),
-    path('', include('sellers.urls')),
-    path('', include('products.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^admin/', admin.site.urls),
+    path(r'', include(router.urls)),
+    # url('', include('sellers.urls')),
+    # url('', include('products.urls')),
+    url(r'^(?P<path>.*)/$', FrontendRenderView.as_view()),
+]
 
 urlpatterns += [
-    re_path(r'(?P<path>.*)', FrontendRenderView.as_view()),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
