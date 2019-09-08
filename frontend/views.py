f77253c90ebf99774eaa4c97414a092018dceb96
Sebastian Strobl <Sebastian@Strobl.net>
2019-09-08 23:55:15 +0200

    annoying mistakes and pointless anyways => decoupled angular from Django

--- frontend/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import View
-
-
-class FrontendRenderView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'frontend/angular/index.html', {})
--- project_planner/urls.py
+++ project_planner/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.conf.urls import url, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.shortcuts import render
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework import routers
-from frontend.views import FrontendRenderView
 from projects import views as project_views
 from bills import views as bill_views
 
@@ -12,17 +12,18 @@ router = routers.DefaultRouter()
 router.register('api/projects', project_views.ProjectView)
 router.register('api/bills', bill_views.BillView)
 
+
+def index(request):
+    return render(request, 'frontend/index.html')
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path(r'', include(router.urls)),
     # url('', include('sellers.urls')),
     # url('', include('products.urls')),
-    url(r'^(?P<path>.*)/$', FrontendRenderView.as_view()),
-]
-
-urlpatterns += [
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
 
