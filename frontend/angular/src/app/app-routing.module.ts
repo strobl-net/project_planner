7419ed46cdb777e769dae1ffa0832e6cc2220a5e
Sebastian Strobl <Sebastian@Strobl.net>
2019-09-08 17:39:11 +0200

    added a page not found component

--- frontend/angular/src/app/app-routing.module.ts
+++ frontend/angular/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent} from "./components/pages/home/home.component";
 import { ProfileComponent} from "./components/pages/profile/profile.component";
+import { NotfoundComponent} from "./components/pages/notfound/notfound.component";
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'profile', component: ProfileComponent}
+  {path: 'profile', component: ProfileComponent},
+  {path: '**', component: NotfoundComponent},
 ];
 
 @NgModule({
--- frontend/angular/src/app/app.module.ts
+++ frontend/angular/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './components/skeletal/nav/nav.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { ProfileComponent } from './components/pages/profile/profile.component';
+import { NotfoundComponent } from './components/pages/notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    ProfileComponent
+    ProfileComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
