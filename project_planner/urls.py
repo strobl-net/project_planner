e22c34ff23ad5de4c7e605c27dc33282d95f2316
Sebastian Strobl <Sebastian@Strobl.net>
2019-09-08 20:41:50 +0200

    reverted small error

--- project_planner/urls.py
+++ project_planner/urls.py
@@ -5,13 +5,12 @@ from django.conf.urls.static import static
 from frontend.views import FrontendRenderView
 
 urlpatterns = [
-
+    path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
     path('', include('projects.urls')),
     path('', include('bills.urls')),
     path('', include('sellers.urls')),
     path('', include('products.urls')),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [
