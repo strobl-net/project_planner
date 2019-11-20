d02a7d11a11e82161302dd5eb3287c371ef8dcdd
Sebastian Strobl <Sebastian@Strobl.net>
2019-11-20 22:08:56 +0100

    added product service + product searching on bill creation

--- frontend/angular/src/app/components/pages/bills/bills.component.ts
+++ frontend/angular/src/app/components/pages/bills/bills.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {BillService} from "../../../services/bills/bill.service";
 import {Bill} from "../../../services/bills/bill.model.temp";
+import {ItemService} from "../../../services/items/item.service";
 
 
 @Component({
@@ -12,11 +13,13 @@ export class BillsComponent implements OnInit {
 
   public bills: Bill[];
   public new_bill: Bill;
+  public current_search: string;
+  public items: any[];
 
   bills_loading: boolean = true;
 
 
-  constructor(private bill_service: BillService) {
+  constructor(private bill_service: BillService, private itemService: ItemService) {
     this.bill_service = bill_service;
   }
 
@@ -36,6 +39,23 @@ export class BillsComponent implements OnInit {
         console.log(error);
       })
   };
+
+  getFilteredProducts = (filter: string) => {
+    this.itemService.getFiltered(this.current_search).subscribe(
+      data => {
+        this.items = data;
+        console.log("-=-=-=-=-=-=-=-=-=-=-");
+        console.log(this.items);
+        console.log("-=-=-=-=-=-=-=-=-=-=-");
+      },
+      error => {
+        console.log(error);
+      })
+  };
+
+  updateSearchProduct() {
+    this.getFilteredProducts(this.current_search)
+  }
 
   createNewBill() {
 
