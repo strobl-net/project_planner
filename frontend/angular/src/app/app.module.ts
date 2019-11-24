884a0adba9529acc547b6bcb6772434886626bf5
Sebastian Strobl <Sebastian@Strobl.net>
2019-11-24 17:08:26 +0100

    removed the old navbar import

--- frontend/angular/src/app/app.module.ts
+++ frontend/angular/src/app/app.module.ts
@@ -3,7 +3,6 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {NavComponent} from './components/skeletal/nav/nav.component';
 import {HomeComponent} from './components/pages/home/home.component';
 import {ProfileComponent} from './components/pages/profile/profile.component';
 import {NotfoundComponent} from './components/pages/notfound/notfound.component';
