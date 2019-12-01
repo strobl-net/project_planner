347129005e9f72a904dea52d789f86aa1b4fea39
Sebastian Strobl <Sebastian@Strobl.net>
2019-12-02 00:50:16 +0100

    added seller service

---
+++ frontend/angular/src/app/services/sellers/seller.model.ts
@@ -0,0 +1,5 @@
+export class Seller {
+  id: number;
+  name: string;
+  area: string;
+}
---
+++ frontend/angular/src/app/services/sellers/seller.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Seller} from "./seller.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SellerService {
+
+  private url: string = "http://127.0.0.1:8080/api/sellers/";
+  private search_url: string = "http://127.0.0.1:8080/api/sellers?";
+  private http_headers = new HttpHeaders({'Content-Type': 'application/json'});
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): Observable<Seller[]> {
+    return this.http.get<Seller[]>(this.url);
+  }
+
+  getByID(id: number): Observable<Seller> {
+    return this.http.get<Seller>(this.url + id, {headers: this.http_headers});
+  }
+
+  create(new_seller: Seller): Observable<any> {
+    const body = {
+      name: new_seller.name,
+      area: new_seller.area
+    };
+    return this.http.post(this.url, body, {headers: this.http_headers});
+  }
+
+  getSearched(parameter: string): Observable<Seller[]> {
+    return this.http.get<Seller[]>(this.search_url + "search=" + parameter);
+  }
+
+  getByName(parameter: string): Observable<Seller[]> {
+    return this.http.get<Seller[]>(this.search_url + "name=" + parameter);
+  }
+}
